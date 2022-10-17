ec219cc0bb78da62f150df7b5ee159625619cf81
Pablo Yubero <pabloyub@hotmail.com>
2022-10-17 13:50:28 +0200

    smol xges

--- functional_clusters/clade_enrichment_generate.py
+++ functional_clusters/clade_enrichment_generate.py
@@ -65,7 +65,13 @@ ft = np.array([ line[1:16] for line in ft ])
 nleafs    = len(leaf_names)
 nclusters = len(clade_ids)
 nfeatures = ft.shape[1]
+#...
+clade_nleafs= np.array( [len(leafs) for leafs in clade_lfs])
+genus_list  = np.array( [ get_genus(name) for name in leaf_names] )
+species_list= np.array( [ get_species(name) for name in leaf_names] )
 
+
+#%%
 # ... create true feature matrix
 F = np.zeros((nleafs, nfeatures))*np.nan
 
@@ -106,10 +112,6 @@ for ii, cluster_leafs in tqdm(enumerate(clade_lfs), total=len(clade_lfs)):
     
     
 # Compute univocity
-clade_nleafs= np.array( [len(leafs) for leafs in clade_lfs])
-genus_list  = np.array( [ get_genus(name) for name in leaf_names] )
-species_list= np.array( [ get_species(name) for name in leaf_names] )
-
 S = np.zeros((nclusters,))*np.nan
 for ii, cluster_leafs in enumerate(clade_lfs):
     cluster_taxonomy = np.array( [ genus_list[idx] for idx in cluster_leafs if genus_list[idx] is not None ] )
@@ -126,60 +128,50 @@ np.savez(FILENAME_OUT,
          ZSCORES=ZSCORES)  
     
 
+#%%
 
-plt.figure( figsize=(15,10), dpi=300)    
-for jj in range(15):
-    plt.subplot(3,5,jj+1)
-    plt.plot(1/S, np.abs(ZSCORES[:,jj]),'.')
-    plt.xlabel('Univocity (1/S)')
-    plt.ylabel('abs(Z-score) \n %s' % col_names[jj] )
-    plt.xlim(-0.05, 5.5 )
-    plt.ylim(0,5)
-plt.tight_layout()
+# Entropy/Univocity correlates with the number of leafs within clade
+# plt.figure( figsize=(6,4), dpi=300)
+# plt.plot(S, clade_nleafs,'.')
+# plt.xlabel('Diversity (S)')
+# plt.ylabel('# of terminal leafs')    
 
 
 
-plt.figure( figsize=(6,4), dpi=300)
-plt.plot(S, clade_nleafs,'.')
-plt.xlabel('Diversity (S)')
-plt.ylabel('# of terminal leafs')    
-
-
-
-
-plt.figure( figsize=(15,8), dpi=300)    
-for jj in range(15):
-    plt.subplot(3,5,jj+1)
-    plt.scatter( clade_nleafs, ZSCORES[:,jj], s=8); 
-    xlim = plt.xlim()
+# Massive plot of z-scores
+# plt.figure( figsize=(15,8), dpi=300)    
+# for jj in range(15):
+#     plt.subplot(3,5,jj+1)
+#     plt.scatter( clade_nleafs, ZSCORES[:,jj], s=8); 
+#     xlim = plt.xlim()
     
-    plt.hlines(2, xlim[0], xlim[1] ,'r', lw=0.7)
-    plt.hlines(-2, xlim[0], xlim[1] ,'r', lw=0.7)
-    plt.grid()
+#     plt.hlines(2, xlim[0], xlim[1] ,'r', lw=0.7)
+#     plt.hlines(-2, xlim[0], xlim[1] ,'r', lw=0.7)
+#     plt.grid()
     
-    if jj>9:
-        plt.xlabel('# terminal leafs')
+#     if jj>9:
+#         plt.xlabel('# terminal leafs')
         
-    if np.mod(jj,5)==0:
-        plt.ylabel('abs(Z-score)')
+#     if np.mod(jj,5)==0:
+#         plt.ylabel('abs(Z-score)')
         
-    plt.title('%s' % col_names[jj])
-    # plt.xlim(-0.05, 5.5 )
-    # plt.ylim(0,5)
+#     plt.title('%s' % col_names[jj])
+#     # plt.xlim(-0.05, 5.5 )
+#     # plt.ylim(0,5)
 
-plt.suptitle('Clade enrichment')
-plt.tight_layout()
-
-
+# plt.suptitle('Clade enrichment')
+# plt.tight_layout()
 
 
-# # Find unique patterns
-# THOLD_Z = 3
-# barcode = 1*(ZSCORES>THOLD_Z) - 1*(ZSCORES<-THOLD_Z)
 
-# unq_bc = np.unique(barcode, axis=0)
-# ref_bc = unq_bc[8]
-# idc = np.argwhere( np.all(barcode==ref_bc, axis=1))[:,0]
+
+# Find unique patterns
+THOLD_Z = 3
+barcode = 1*(ZSCORES>THOLD_Z) - 1*(ZSCORES<-THOLD_Z)
+
+unq_bc = np.unique(barcode, axis=0)
+ref_bc = unq_bc[8]
+idc = np.argwhere( np.all(barcode==ref_bc, axis=1))[:,0]
 
 
 
--- functional_clusters/trees_comparison.py
+++ functional_clusters/trees_comparison.py
@@ -16,6 +16,7 @@ FILENAME_CLADE_DATA= 'data_potF_all_clades.npz'
 FILENAME_TREE_16s  = 'tree_16s_labelled.newick'
 FILENAME_TREE_GENE = 'tree_potF_labelled.newick' 
 FILENAME_OUT = 'data_tree_comparison.npz'
+
 def search_accession(name):
     if (name[0] == None) or (name[1] == None):
         return None
@@ -39,7 +40,7 @@ def get_genus(name):
     else: 
         return sp_name
     
-
+#%%
 # Load all clade data
 data = np.load(FILENAME_CLADE_DATA, allow_pickle=True)
 leaf_names = data['leaf_names']
@@ -123,12 +124,31 @@ np.savez(FILENAME_OUT, n_bichos = n_bichos,
                        leafs_idx_lca = leafs_idx_lca)
 
 
+#%% PLOTS
+
+# Load tree data
+data = np.load( FILENAME_OUT, allow_pickle=True)
+n_bichos = data['n_bichos']
+depth_lca_16s = data['depth_lca_16s']
+depth_lca_gene = data['depth_lca_gene']
+leafs_idx_lca = data['leafs_idx_lca']
+
+# Load enrichment data
+data = np.load('data_clade_enrichment.npz')
+F = data['F']
+S = data['S']
+ZSCORES = data['ZSCORES']
+idx_significant = np.argwhere( np.any(np.abs(ZSCORES)>3, axis=1))[:,0]
+
 
 depth_lca_16s += np.random.randn( *depth_lca_16s.shape )/50000
 depth_lca_gene+= np.random.randn( *depth_lca_gene.shape )/50000
 
 plt.figure( figsize=(6,4), dpi=300)      
-plt.scatter( depth_lca_16s, depth_lca_gene,c = np.log10(1+np.array(n_bichos)), s=6)    
+plt.plot(  depth_lca_16s, depth_lca_gene ,'.', color=np.ones((3,))*0.5, ms=5, zorder=0 )
+plt.scatter( depth_lca_16s[idx_significant], depth_lca_gene[idx_significant],c = np.log10(1+np.array(n_bichos[idx_significant])), s=6)    
+
+
 plt.colorbar( label=' log10 Number of leafs in potF')
 plt.xlabel('Taxonomic relatedness')
 plt.ylabel('Relatedness')
