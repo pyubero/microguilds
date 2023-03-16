65df8d213b92504bdd1d0de7b830453ebbd344ee
Pablo Yubero <pabloyub@hotmail.com>
2023-03-16 17:52:20 +0100

    Update uGuilds_plots.py
    Added hatches and cyclic coloring.
    By setting DISPLAY_MODE you can now display everything in either linear or log10

--- brujulas/uGuilds_plots.py
+++ brujulas/uGuilds_plots.py
@@ -141,7 +141,14 @@ def stackbar(ax, x, Y, *args, **kwargs):
     else:
         _given_colors = False
         
-    if len(Y)==0:
+    if "hatches" in kwargs:
+        hatches = kwargs["hatches"]
+        kwargs.pop("hatches")
+        _given_hatches = True
+    else:
+        _given_hatches = False
+        
+    if Y.shape[0] == 0:
         return ax, kwargs["bottom"]
         
     for idx in range(Y.shape[0]):
@@ -149,7 +156,12 @@ def stackbar(ax, x, Y, *args, **kwargs):
             kwargs.update({"label" : labels[idx]})
         
         if _given_colors:
-            kwargs.update( {"color" : colors[idx,:] })
+            idx_color = idx % colors.shape[0]
+            kwargs.update( {"color" : colors[idx_color,:] })
+            if _given_hatches:
+                idx_hatch = idx // colors.shape[0]
+                kwargs.update( {"hatch" : hatches[idx_hatch]})
+        
         
         # Plot
         ax.bar( x, Y[idx,:], *args, **kwargs)
@@ -188,25 +200,28 @@ def save_figure(HFigure, filepath, overwrite=True, *args, **kwargs):
 # General variables
 GENE_NAME = 'potF'
 LEVEL_NAME = 'Species_GTDB'
+DISPLAY_MODE = 'log10' #  ... either linear or log10
 OVERWRITE = False
 VERBOSE = True
-
-
+#...
 _filename = f'kvalues_{GENE_NAME}_{LEVEL_NAME}.tsv'
 out_filename = f"gpattern_{GENE_NAME}_{LEVEL_NAME}.png"
 
-#... plotting options
-MAX_TAXONS_SHOWN = 20
+# Plotting options
+MAX_TAXONS_SHOWN = 30
 TAXONOMIC_ADVANTAGE = 0 # taxons fully assigned advantage over worse identifiable taxons
 
-#... some styling options
+# Styling options
 DPI = 200
+N_COLORS = 20
 R_UPPER_MARGIN_REL = 1.02 # radial margin top, relative to the maximum
 BAR_WIDTH_REL = 0.5
 COLOR_UNASSIGNED= [0.0, 0.0, 0.0, 1]
 COLOR_OTHERS = [0.3, 0.3, 0.3, 1]
 
 
+#######################################################
+###################### LOAD DATA ######################
 df = pd.read_csv(_filename, sep='\t', header=0)
 # ...
 taxons = df['Taxon'].unique()
@@ -217,68 +232,71 @@ K = from_df_to_ktensor(df, [taxons, contexts, clusters], column="k-value").astyp
 # ...
 n_taxons, n_ctxts, n_clusters = K.shape
 
-
-## Find plottable DATA ##
-contrib_per_taxon = np.sum(np.sum(K, axis=2), axis=1)
 verboseprint(f"Loaded data for:")
 verboseprint(f"   {n_taxons:4d}\ttaxons")
 verboseprint(f"   {n_ctxts:4d}\tcontexts")
 verboseprint(f"   {n_clusters:4d}\tclusters.")
 
 
+#######################################################
+#################### ANALYZE DATA #####################
+## Find plottable DATA ##
+contrib_per_taxon = np.sum(np.sum(K, axis=2), axis=1)
+
 # All UNASSIGNED taxons add to the class "Unassigned"
 # ... we compute the taxonomic scores
 tx_scores = np.array([score_taxon(tx) for tx in taxons])
-# idx_unassigned = np.argwhere(tx_scores<0.5)[:,0]
 idx_unassigned = np.argwhere( np.array(taxons) =="s__")[:,0]
-verboseprint(f"Found {len(idx_unassigned)} unassigned taxons:")
-_ = [verboseprint(f"   {tx}") for tx in taxons[idx_unassigned] ]
-verboseprint("")
-
+# idx_unassigned = np.argwhere(tx_scores<0.5)[:,0]
 
 # All taxons in the top MAX_TAXONS_SHOWN add to the class "Show"
-idc_left = list(set(range(n_taxons)) - set(idx_unassigned)) #- set(idx_below_min)
+idc_left = list(set(range(n_taxons)) - set(idx_unassigned))
 _maxshown = np.clip(MAX_TAXONS_SHOWN,0, len(idc_left))
 scores = np.log10( contrib_per_taxon ) + tx_scores*TAXONOMIC_ADVANTAGE
 threshold = np.sort(scores[ idc_left ])[-_maxshown]
 idx_show = np.argwhere( scores[ idc_left ]>=threshold)[:,0]
 idx_show = np.array( idc_left )[idx_show]
 
-
 # All taxons left add to the class "Others"
 idx_other = list( set(idc_left) - set(idx_show) )
-verboseprint(f"Will group {len(idx_other)} taxons into 'Others'.")
-  
 
+# Build K matrix to be shown, by summing the Kvalues of others, and unassigned.  
 K_shown = np.zeros((len(idx_show)+2, n_ctxts, n_clusters) )
 K_shown[0,:,:] = np.sum(K[idx_other,:,:], axis=0)
 K_shown[1,:,:] = np.sum(K[idx_unassigned,:,:], axis=0)
 K_shown[2:,:,:]= K[idx_show,:,:]
 k_min = contrib_per_taxon[idx_show].min()
-
 taxon_labels = [f"Others, K<{k_min:1.1f}","Unassigned", *taxons[idx_show]]
 
+
+verboseprint(f"Found {len(idx_unassigned)} unassigned taxons:")
+_ = [verboseprint(f"  {tx}") for tx in taxons[idx_unassigned] ]
+verboseprint("")
+verboseprint(f"Will group {len(idx_other)} taxons into 'Others'.")
+verboseprint(f"Showing the top {len(idx_show)} contributing taxons, with")
+verboseprint(f"  K-values >= {k_min:1.3f}.")
+verboseprint("")
 
 # General plotting properties
 #... style properties and labels 
 # clst_labels = [ 'F. '+intToRoman( _+1) for _ in range( n_clusters ) ]
-colors = plt.cm.tab20( np.linspace(0, 1, MAX_TAXONS_SHOWN) )
-colors = colors[cycle_through(colors, 20),:]
+colors = plt.cm.tab20( np.linspace(0, 1, N_COLORS) )
 colors = np.concatenate(([COLOR_OTHERS,], colors), axis=0)
 colors = np.concatenate(([COLOR_UNASSIGNED,], colors), axis=0)
-mpl.rcParams['hatch.linewidth'] = 0.3  # previous pdf hatch linewidth
-hatches=[ '' , '////////' ,'........']
+mpl.rcParams['hatch.linewidth'] = 0.4  # previous pdf hatch linewidth
+hatches=[ '' , '//////////' ,'..........']
 
 
 
-#########################
-######### Plots #########
-# Linear version
-# X = K.copy()
-# Log version
-X = np.log10( K_shown )
-X[X<0] = 0
-
+#######################################################
+###################### PLOT DATA ######################
+if DISPLAY_MODE == "linear":
+    X = K.copy()
+    verboseprint("Currently displaying stacked k_values, in LINEAR mode.")
+elif DISPLAY_MODE == "log10":
+    X = np.log10( K_shown )
+    X[X<0] = 0
+    verboseprint("Currently displaying stacked log10(k_values), in LOG10 mode.")
 
 
 #... derived values and matrices
@@ -302,10 +320,22 @@ bottoms = np.zeros((n_ctxts, n_clusters))
 for _context in range( n_ctxts ):
     ax = plt.subplot( n_rows, n_cols, _context+1, projection='polar')    
 
-    ax, _ = stackbar(ax, theta, X[:, _context,:],
+    # Plot Others and Unassigned
+    ax, _bot = stackbar(ax, theta, X[:2, _context,:],
                        width=width,
-                       colors=colors,
-                       labels=taxon_labels)
+                       colors=colors[:2,:],
+                       hatches=hatches,
+                       labels=taxon_labels[:2])
+
+
+    # Plot the rest, nicely with hatches and colors
+    bottoms[_context, :] = _bot
+    ax, _ = stackbar(ax, theta, X[2:, _context,:],
+                       width=width,
+                       colors=colors[2:],
+                       hatches=hatches,
+                       labels=taxon_labels[2:],
+                       bottom = bottoms[_context, :])
 
     ax.set_rgrids( np.ceil(rlims[1:]), labels='' )
     ax.set_thetagrids( theta*57.3, labels=clusters, fontsize=8)
@@ -320,6 +350,8 @@ ax = plt.subplot(1, n_ctxts+1, n_ctxts+1)
 plt.axis('off')
 legend.set_bbox_to_anchor( (2.1, 1.25) )
 
+verboseprint(f"The radial units are:")
+verboseprint(f"{np.ceil(rlims)}")
 
 # Save figure
 save_figure(HFigure, out_filename, overwrite=OVERWRITE, dpi=DPI )
